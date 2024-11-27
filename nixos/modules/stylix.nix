5d994d92d7a8474d098c2a7da733c9dacd24841d
andrey_varnavskiy <andrey_varnavskiy@student.itpu.uz>
2024-11-27 10:31:40 +0500

    dark polarity

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -2,6 +2,7 @@
   imports = [ inputs.stylix.nixosModules.stylix ];
   stylix = {
     enable = true;
+    polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
   };
 }
