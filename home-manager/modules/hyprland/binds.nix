df455bf3c5a7aaaccdf63df2f703a29a7b57ead0
andrey_varnavskiy <andrey_varnavskiy@student.itpu.uz>
2024-11-28 05:17:35 +0500

    add brightness control

--- home-manager/modules/hyprland/binds.nix
+++ home-manager/modules/hyprland/binds.nix
@@ -79,8 +79,8 @@
       ",XF86AudioLowerVolume,  exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
       ",XF86AudioMute,         exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
       ",XF86AudioMicMute,      exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ",XF86MonBrightnessUp,   exec, brightnessctl s 10%+"
-      ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+      "$mainMod, bracketright, exec, brightnessctl s 10%+"
+      "$mainMod, bracketleft,  exec, brightnessctl s 10%-"
     ];
 
     # Audio playback
