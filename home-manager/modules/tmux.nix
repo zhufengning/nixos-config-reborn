727ac2e85a7a1e14a87f61ed7fb49918770dbf07
andrey_varnavskiy <andrey_varnavskiy@student.itpu.uz>
2024-11-29 05:31:18 +0500

    remove unnecessary plugins

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -36,21 +36,18 @@
       bind -n M-Q kill-session
     '';
     plugins = with pkgs; [
-      tmuxPlugins.cpu
-      {
-        plugin = tmuxPlugins.gruvbox;
-      }
-      {
-        plugin = tmuxPlugins.resurrect;
-        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
-      }
-      {
-        plugin = tmuxPlugins.continuum;
-        extraConfig = ''
-      set -g @continuum-restore 'on'
-      set -g @continuum-save-interval '60' # minutes
-        '';
-      }
+      tmuxPlugins.gruvbox
+      # {
+      #   plugin = tmuxPlugins.resurrect;
+      #   extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      # }
+      # {
+      #   plugin = tmuxPlugins.continuum;
+      #   extraConfig = ''
+      # set -g @continuum-restore 'on'
+      # set -g @continuum-save-interval '60' # minutes
+      #   '';
+      # }
     ];
   };
 }
