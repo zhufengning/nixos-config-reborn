b8eeca1f66be028f6f04762400c8ed86e568947c
andrey_varnavskiy <andrey_varnavskiy@student.itpu.uz>
2024-12-01 06:17:51 +0500

    NIXOS_OZONE_WL="1"

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -3,7 +3,7 @@
     TERMINAL = "alacritty";
     EDITOR = "nvim";
     # Hint Electron apps to use Wayland
-    # NIXOS_OZONE_WL = "1";
+    NIXOS_OZONE_WL = "1";
     XDG_BIN_HOME = "$HOME/.local/bin";
     PATH = [
       "${XDG_BIN_HOME}"
