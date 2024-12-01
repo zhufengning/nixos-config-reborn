b729a029328e00476e7fbba6a5ecc92f4986d2d1
andrey_varnavskiy <andrey_varnavskiy@student.itpu.uz>
2024-12-01 06:16:47 +0500

    grace 300 => 10

--- home-manager/modules/hyprland/hyprlock.nix
+++ home-manager/modules/hyprland/hyprlock.nix
@@ -4,7 +4,7 @@
     settings = {
       general = {
         disable_loading_bar = true;
-        grace = 300;
+        grace = 10;
         hide_cursor = true;
         no_fade_in = false;
       };
