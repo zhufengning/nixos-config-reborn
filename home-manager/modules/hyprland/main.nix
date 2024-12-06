05735e80e31005be16cf68a6031887d773072b79
andrey_varnavskiy <andrey_varnavskiy@student.itpu.uz>
2024-12-06 16:20:59 +0500

    add env vars

--- home-manager/modules/hyprland/main.nix
+++ home-manager/modules/hyprland/main.nix
@@ -2,6 +2,16 @@
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
+      env = [
+        # Hint Electron apps to use Wayland
+        "NIXOS_OZONE_WL,1"
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+        "QT_QPA_PLATFORM,wayland"
+        "XDG_SCREENSHOTS_DIR,~/screens"
+      ];
+
       monitor = ",1920x1080@60,auto,1";
       "$mainMod" = "SUPER";
       "$terminal" = "alacritty";
