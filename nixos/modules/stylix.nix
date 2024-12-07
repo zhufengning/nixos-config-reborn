b1566315f19b10d9ec6ddf0276012b75936b5a51
andrey_varnavskiy <andrey_varnavskiy@student.itpu.uz>
2024-12-07 14:24:34 +0500

    wallpaper

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -4,5 +4,11 @@
     enable = true;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+
+    # This doesn't work and idk why
+    # image = pkgs.fetchurl {
+    #   url = "hhttps://codeberg.org/lunik1/nixos-logo-gruvbox-wallpaper/raw/branch/master/png/gruvbox-dark-rainbow.pngttps://codeberg.org/lunik1/nixos-logo-gruvbox-wallpaper/raw/branch/master/png/gruvbox-dark-rainbow.png";
+    #   sha256 = "036gqhbf6s5ddgvfbgn6iqbzgizssyf7820m5815b2gd748jw8zc";
+    # };
   };
 }
