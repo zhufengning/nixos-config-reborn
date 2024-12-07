748630138481f4764fda5a2093fe8cdc4ce71a05
andrey_varnavskiy <andrey_varnavskiy@student.itpu.uz>
2024-12-07 14:23:29 +0500

    small rework

--- home-manager/modules/obsidian.nix
+++ home-manager/modules/obsidian.nix
@@ -1,11 +1,10 @@
-{ user, pkgs, ... }:
+{ pkgs, ... }:
 let
-  vaultDir = "/home/${user}/para";
   gitSyncObsidian = pkgs.writeScriptBin "git-sync-obsidian" ''
     #!/bin/sh
 
-    VAULT_DIR="${vaultDir}"
-    cd "$VAULT_DIR" || exit 1
+    VAULT_DIR="$HOME/para"
+    cd $VAULT_DIR || exit 1
     git add .
     git commit -m "$(date '+%Y-%m-%d %H:%M:%S')" || exit 0
   '';
@@ -13,20 +12,19 @@ in {
   home.packages = [ gitSyncObsidian ];
 
   systemd.user.services.git-sync-obsidian = {
-    Unit.Description = "Commit Obsidian Vault changes with git";
+    Unit = {
+      Description = "Sync Obsidian Vault with GitHub";
+      Wants = "git-sync-obsidian.timer";
+    };
     Service = {
       ExecStart = "${gitSyncObsidian}/bin/git-sync-obsidian";
       Type = "simple";
     };
-    Install.WantedBy = [ "timers.target" ];
   };
 
   systemd.user.timers.git-sync-obsidian = {
     Unit.Description = "Run Git Sync for Obsidian Vault";
-    Timer = {
-      OnCalendar = "*:0/15";
-      Persistent = true;
-    };
+    Timer.OnCalendar = "*:0/15";
     Install.WantedBy = [ "timers.target" ];
   };
 }
