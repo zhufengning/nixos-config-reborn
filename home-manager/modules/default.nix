8f667e26362b4d68752fa7738502c89e000aec63
andrey_varnavskiy <andrey_varnavskiy@student.itpu.uz>
2024-12-16 04:43:25 +0500

    no fuzzel

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -3,13 +3,11 @@
     ./alacritty.nix
     ./cursor.nix
     ./eza.nix
-    ./fuzzel.nix
     ./git.nix
     ./hyprland
     ./lazygit.nix
     ./neovim.nix
     ./obsidian.nix
-    ./qt.nix
     ./starship.nix
     # ./stylix.nix
     ./swaync
